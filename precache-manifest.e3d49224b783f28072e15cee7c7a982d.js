f15bbc9a9891b0555305453194c06cf00699ccb1
Timothyyt <tian_yang@gwu.edu>
2020-07-14 17:02:29 -0400

    Updates

---
+++ precache-manifest.e3d49224b783f28072e15cee7c7a982d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0f654036af8d99a7ab63a77cbb8f24",
+    "url": "/Todo_list_project/index.html"
+  },
+  {
+    "revision": "38ddc9f0ea3598fad739",
+    "url": "/Todo_list_project/static/css/main.214ffd64.chunk.css"
+  },
+  {
+    "revision": "6b2b764d08db502de1e0",
+    "url": "/Todo_list_project/static/js/2.16751832.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo_list_project/static/js/2.16751832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ddc9f0ea3598fad739",
+    "url": "/Todo_list_project/static/js/main.1ace4157.chunk.js"
+  },
+  {
+    "revision": "020a2dbdf5b3087de00e",
+    "url": "/Todo_list_project/static/js/runtime-main.331b877a.js"
+  }
+]);
